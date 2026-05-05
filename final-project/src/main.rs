83b5be696b475ce29ed80ebd5d64270ffd2d6b30
XEternalKingX <zoeyac02@gmail.com>
2026-05-05 09:50:48 +0000

    Final Project Enviroment Setup

---
+++ final-project/src/main.rs
@@ -0,0 +1,19 @@
+// Concurrent Task Dispatcher, Final Project
+// What we need in our program, from handout:
+//  generate a large collection of tasks
+//  simulate arrival over time
+//  place tasks into a queue or queues
+//  use a bounded worker pool
+//  dispatch tasks according to a scheduling policy
+//  record useful statistics
+//  shut down cleanly
+// You are building a simulation, not a real operating system
+
+
+
+
+
+
+
+
+
